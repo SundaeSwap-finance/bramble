c16a82e4097ee31217dc732825aa01ec85ce0f80
Pi Lanningham <pi.lanningham@gmail.com>
2023-11-18 11:23:28 -0500

    Let the playground use a custom mount point
    Allows setting a custom mount point, and also reads the query mount point from the bramble config

--- plugins/playground.go
+++ plugins/playground.go
@@ -1,6 +1,8 @@
 package plugins
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -13,6 +15,27 @@ func init() {
 
 type PlaygroundPlugin struct {
 	*bramble.BasePlugin
+	queryPath string
+	config    PlaygroundPluginConfig
+}
+
+type PlaygroundPluginConfig struct {
+	Path *string `json:"path,omitempty"`
+}
+
+func (p *PlaygroundPlugin) Configure(cfg *bramble.Config, data json.RawMessage) error {
+	if data == nil {
+		return nil
+	}
+	if err := json.Unmarshal(data, &p.config); err != nil {
+		return err
+	}
+	// We *should* be able to rely on the default being set
+	p.queryPath = "/query"
+	if cfg.GraphqlPath != nil {
+		p.queryPath = *cfg.GraphqlPath
+	}
+	return nil
 }
 
 func (p *PlaygroundPlugin) ID() string {
@@ -20,5 +43,10 @@ func (p *PlaygroundPlugin) ID() string {
 }
 
 func (p *PlaygroundPlugin) SetupPublicMux(mux *http.ServeMux) {
-	mux.HandleFunc("/playground", playground.Handler("Bramble Playground", "/query"))
+	path := "/playground"
+	if p.config.Path != nil {
+		path = *p.config.Path
+	}
+	fmt.Printf("queryPath: %v\n", p.queryPath)
+	mux.HandleFunc(path, playground.Handler("Bramble Playground", p.queryPath))
 }
